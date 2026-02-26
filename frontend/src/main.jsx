efc6f8e3ee90b880b58f1914a24b910cb0a20e38
Deep Chand <deepc2060@gmail.com>
2026-02-26 11:57:21 +0530

    setup Redux store and auth slice

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 import "./index.css";
+import { Provider } from "react-redux";
+import store from "./Redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 );
